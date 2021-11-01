87b504e860fd2f07dc1986fe7d789c84517e0e5c
MaksGovor <maksgovruha@gmail.com>
2021-11-01 17:40:40 +0200

    Add handler impelentation

--- handler.go
+++ handler.go
@@ -1,6 +1,9 @@
 package lab2
 
-import "io"
+import (
+	"bufio"
+	"io"
+)
 
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
@@ -10,6 +13,24 @@ type ComputeHandler struct {
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	scanner := bufio.NewScanner(ch.Input)
+	writter := bufio.NewWriter(ch.Output)
+
+	for scanner.Scan() {
+		exp := scanner.Text()
+		res, err := PrefixToInfix(exp)
+		if err != nil {
+			return err
+		}
+
+		if _, err := writter.WriteString(res + "\n"); err != nil {
+			return err
+		}
+
+		if err := writter.Flush(); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
