dc67fab7c38518688e80779a9b5a8c9dd29c73f6
YAGoOaR <yegorgribenko68@gmail.com>
2021-11-05 16:23:24 +0200

    Refactor implementation.go

--- implementation.go
+++ implementation.go
@@ -5,10 +5,11 @@ import (
 	"regexp"
 )
 
-func slicePop(arr []string) ([]string, string) {
-	l := len(arr)
-	item := arr[l-1]
-	return arr[:l-1], item
+func slicePop(arr *[]string) string {
+	l := len(*arr)
+	item := (*arr)[l-1]
+	*arr = (*arr)[:l-1]
+	return item
 }
 
 func reverseSlice(slice []string) []string {
@@ -50,16 +51,13 @@ func PrefixToInfix(input string) (string, error) {
 		savedItems := []string{}
 
 		for len(tempResult) >= 4 && !symbolItem.MatchString(tempResult[len(tempResult)-1-2]) {
-			var lastElem string
-			tempResult, lastElem = slicePop(tempResult)
+			lastElem := slicePop(&tempResult)
 			savedItems = append(savedItems, lastElem)
 		}
 
 		curItems := []string{tempResult[len(tempResult)-1], tempResult[len(tempResult)-2]}
 		tempResult = tempResult[:len(tempResult)-2]
-
-		var curSymbol string
-		tempResult, curSymbol = slicePop(tempResult)
+		curSymbol := slicePop(&tempResult)
 
 		if symbolNeedsBracket.MatchString(curSymbol) {
 			for i := 0; i < len(curItems); i++ {
