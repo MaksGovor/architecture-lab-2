bcafd1cb8feec2ae798cc9a73b8f754a87423b39
MaksGovor <maksgovruha@gmail.com>
2021-12-20 22:58:28 +0200

    Fix err testing on benchmark

--- implementation_test.go
+++ implementation_test.go
@@ -153,10 +153,10 @@ func ExamplePrefixToInfix() {
 var res string
 
 func BenchmarkPrefixToInfix(b *testing.B) {
-	str := "- * / 15 - 7 + 1 1 3 + 2 + 1 1 "
+	str := "- * / 15 - 7 + 1 1 3 + 2 + 1 + 9 "
 	for i := 1; i <= 100; i++ {
-		testStr := strings.Repeat(str, i)
-		b.Run(fmt.Sprintf("operands %d", i*8), func(b *testing.B) {
+		testStr := strings.Repeat(str, i) + "1"
+		b.Run(fmt.Sprintf("%d-operators", i*6), func(b *testing.B) {
 			res, _ = PrefixToInfix(testStr)
 		})
 	}
